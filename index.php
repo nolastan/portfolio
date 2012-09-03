3ad23fcf2f67515235f95a39a80b34233f9d90b1
Stanford Rosenthal <nolastan@gmail.com>
2012-09-02 21:33:08 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+define('PROJECT_NAME', 'Stanford Rosenthal');
+define('COMPANY_NAME', 'rosenthal.co');
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="reset.css" />
+	<link rel="stylesheet" href="one-page.css" />
+	<link rel="stylesheet" href="style.css" />
+	<title>Stanford Rosenthal</title>
+</head>
+<body>
+<div id="container">
+	<header>
+		<h1 id="project_name"><?=PROJECT_NAME?></h1>
+		<h2 id="company_name"><?=COMPANY_NAME?></h2>
+		<div class="clear"></div>
+	</header>
+	<div id="content">
+		<h3 id="project_description">I'm currently at <a href="http://www.taskrabbit.com/PAL/66898">TaskRabbit</a> in San&nbsp;Francisco.</h3>
+		
+		<p>I am passionate about using the Internet and wonderful design to empower communities.</p>
+				
+		<p>I'm not currently looking for opportunities. But if you want to change the world, you should <a href="mailto:stan@rosenthal.co">email me</a>.</p>
+				
+		
+<!-- 		<div class="resume"><a href="resume.pdf" class="large green button" >Download my resume</a></div> -->
+		<h3 style="margin-top:1.5em">Stan everywhere</h3>
+		<ul class="social">
+			<li><a href="http://twitter.com/nolastan" title="Follow me on Twitter"><img src="images/twitter-2.png" alt="Twitter" /></a></li>
+			<li><a href="http://www.linkedin.com/in/stanfordrosenthal"><img src="images/linkedin.png" alt="Twitter" /></a></li>
+			<li><a href="http://github.com/nolastan" title="See my activity on Github"><img src="images/github.png" alt="Github" /></a></li>
+			<li><a href="http://www.flickr.com/photos/nolastan/" title="See my photos on Flickr"><img src="images/flickr.png" alt="Flickr" /></a></li>
+			<li><a href="http://delicious.com/standaman2009" title="View my links on Delicious"><img src="images/delicious.png" alt="Del.icio.us" /></a></li>
+		</ul>
+	</div>
+	<ul id="sidebar">
+		<li class="projects">
+<!-- 			<h3>Things</h3> -->
+			<ul id="projects">
+				<li>
+					<a href="chopd"><img src="images/chopd.png" alt="" /></a>
+					<h4>Chopd</h4>
+					<p>A modular typeface</p>
+				</li>
+				<li>
+					<a href="https://github.com/codeforamerica/open311_facebook"><img src="images/open311.png" alt="" /></a>
+					<h4>Open311 for Facebook</h4>
+					<p>A Facebook app for citizens to report non-emergency requests to city governments.</p>
+				</li>
+				<li>
+					<a href="non-profit-social-media.php"><img src="images/levees.png" alt="" /></a>
+					<h4>Levees.Org</h4>
+					<p>An organization I founded with my mom after the 2005 levee failures devastated New Orleans.</p>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
+<footer>
+<p>I eat buzzwords for breakfast.</p>
+</footer>
+
+</body>
+</html>
---
+++ non-profit-social-media.php
@@ -0,0 +1,76 @@
+<?php
+define('PROJECT_NAME', 'Stanford Rosenthal');
+define('COMPANY_NAME', 'rosenthal.co');
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="reset.css" />
+	<link rel="stylesheet" href="one-page.css" />
+	<title>Stanford Rosenthal</title>
+</head>
+<body>
+<div id="container" class="article">
+<header class="clearfix">
+	<h1 id="project_name"><?=PROJECT_NAME?></h1>
+	<h2 id="company_name"><a href="/"><?=COMPANY_NAME?></a></h2>
+</header>
+<div id="content">
+<article>
+	<h1>Social Media for Non-Profit</h1>
+	<h2>By <a href="http://stanfordrosenthal.com">Stanford Rosenthal</a> on November 18th, 2009</h2>
+
+	<h3>Background</h3>
+	<p><a href="http://levees.org">Levees.Org</a> is a not-for-profit grassroots organization that my mother and I founded in December of 2005 while we were exiled from New Orleans.  Our mission is education that the catastrophic flooding of New Orleans was a <a href="http://www.upi.com/Top_News/2006/04/06/Corps-of-Engineers-admits-design-failure/UPI-91851144355032/">man-made federal disaster</a>, not a natural one.  Today we have over 24,000 members and chapters in six states.</p>
+	<p>I have decided to share what we have learned in order to help other organizations and causes.  It's important to note that none of this requires a social media professional.  Once everything is setup, the workflow is simple.  (If you need proof, ask my mother!  She's been running our online presence for months now without any assistance from me.)</p>
+	<figure>
+		<a href="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.50.44-PM.png" target="_blank"><img title="Levees.Org Analytics" src="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.50.44-PM.png" alt="Top organic and referral traffic sources for Levees.Org" width="527" height="216" /></a>
+		<figcaption>Top organic and referral traffic sources for Levees.Org.</figcaption>
+	</figure>
+
+	<h3>Conquer Google</h3>
+	<figure class="right">
+		<a href="http://google.com/search?q=levees.org" target="_blank"><img title="Levees.Org on Google" src="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.32.23-PM.png" alt="Google search results for Levees.Org" width="278" height="41" /></a>
+	</figure>
+	<p>We control 8 of the 11 links on the first results page on Google for [<a href="http://www.google.com/search?q=levees.org">levees.org</a>].  How?  Syndication is key:</p>
+	<ol>
+		<li>Syndicate content at all major video sites using <a href="http://tubemogul.com" target="_blank">TubeMogul</a>.</li>
+		<li>Syndicate Twitter and Blog feeds in as many places as possible, such as Facebook, Myspace, Tumblr, Friendfeed, LinkedIn, Plaxo and more.</li>
+		<li>Cross post on other blogs.  We do this on <a href="http://blog.nola.com/levees" target="_blank">NOLA.com</a>.  (Bonus: these posts sometimes appear on Google as "news.")</li>
+		<li>Pay <a href="http://prweb.com" target="_blank">PRweb</a> to publish press releases on various sites.  (<a href="http://www.google.com/search?q=%22Levees.Org+Expands+National+Presence+by+Establishing+Its+Sixth+Chapter%3A+New+York%22&amp;aq=f&amp;oq=&amp;aqi=" target="_blank">example</a>)</li>
+	</ol>
+
+	<h3>Dominate YouTube</h3>
+	<p>We used to host our own videos using a paid service.  Big mistake.  Now we host all of our videos for free on <a href="http://youtube.com/leveesorg" target="_blank">YouTube</a>, where they can go viral.  Consequently, YouTube is our #4 top website referrer, sending mostly new visitors.</p>
+	<p>Our primary goal is always to be featured on the YouTube top-100 lists.  Here's how we get there:</p>
+	<ol>
+		<li>We're never shy about asking our members to login or create accounts at YouTube to rate, "favorite," and comment on our video.  We even use YouTube annotations over our video to point to the rate button.  As a result, our videos are often featured as the highest rated or most commented-on videos.</li>
+		<li>Whenever we upload a video, we immediately do a massive push to drive as much traffic as possible.  We give our members a 24-hour timeframe to watch the video and send to their friends, and we even coordinate these efforts in advance with bloggers.  As a result, we make it to the top-100 "viewed today" page more often than an organization our size should.</li>
+	</ol>
+	<figure>
+		<a target="_blank" href="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.28.55-PM.png"><img title="levees.org number 4" src="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.28.55-PM.png" alt="Screenshot" width="467" height="353" /></a>		
+		<figcaption>Levees.Org takes the #4 spot on YouTube News &amp; Politics, competing with popular topics such as Palin, Obama, and the Viacom/YouTube lawsuit</figcaption>
+	</figure>
+	<h3>Leverage Wikipedia</h3>
+	<p>Most people don't think of Wikipedia in their social media marketing efforts.  They should.</p>
+	<p>Wikipedia is our #2 top traffic source and #1 referrer; nearly 5% of our traffic comes from Wikipedia.  This is because our members have placed <span>Levees</span>.<span>Org</span> links under the external links section of relevant Wikipedia articles.  Along with adding links to articles, we have spent time improving articles and interlinking related articles.</p>
+	<p>We've also created entirely new articles, such as <a href="http://en.wikipedia.org/wiki/Flood_Control_Act_of_1965" target="_blank">Flood Control Act of 1965</a>.  It didn't take much effort; the article started like <a href="http://en.wikipedia.org/w/index.php?title=Flood_Control_Act_of_1965&amp;oldid=126647604" target="_blank">this</a> and was expanded by the community.  In fact, we didn't even add our link to that article; someone else did later on.</p>
+	<figure>
+		<a target="_blank" href="http://en.wikipedia.org/wiki/New_Orleans_Flood#External_links"><img title="Levees.org on Wikipedia" src="http://stanfordrosenthal.com/wp-content/uploads/2009/11/Screen-shot-2009-11-18-at-12.35.35-PM.png" alt="Screenshot" width="447" height="207" /></a>
+		<figcaption>Levees.Org is listed as the first external link in many relevant Wikipedia articles. The YouTube link (#4) is to our video.</figcaption>
+	</figure>
+	<h3>Rethink Conversion Goals</h3>
+	<p>In 2005, all that mattered to us as far as social media was collecting e-mail addresses.  We did a great job at this thanks to the help of Internet Marketing expert Ken McCarthy.  However, eventually we realized (as Ken McCarthy <a href="http://kenmccarthy.com/blog/2009/11/05/skeptic-surrender-twitter-wins/" target="_blank">finally</a> has <a href="http://twitter.com/kenmccarthy" target="_blank">as well</a>) that people aren't using e-mail as their primary form of communication; many young people don't seem to be using it at all.  We now think of gaining Facebook and Twitter followers as important as collecting e-mail addresses.</p>
+	<figure>
+		<img class="size-full wp-image-131" title="Facebook Stats" src="http://stanfordrosenthal.com/wp-content/uploads/2009/11/facebook-stats.png" alt="We redesigned our website to focus on social media." width="446" height="236" />
+		<figcaption>We redesigned our website to focus on social media.</figcaption>
+	</figure>
+</article>
+</div>
+</div>
+<footer>
+<p>Created by <a href="http://stanfordrosenthal.com">Stanford Rosenthal</a>.</p>
+</footer>
+
+</body>
+</html>
